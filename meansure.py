7dd5d0ec42172bd24d787908bb3923b4c15a2396
KaerMorh <1830616305@qq.com>
2023-05-26 17:29:44 +0800

    在程序的最后，需要输出判断正确与错误的个数，如果有错误的，要输出错误视频的名称。

---
+++ meansure.py
@@ -0,0 +1,40 @@
+import json
+
+path = r'D:\0---Program\Projects\aimbot\yolov5-master\yolov5-master\output\20230526170401_log.json'
+# 读取日志文件
+import json
+
+def check_video_logs(path):
+    # 读取log文件
+    with open(path) as json_file:
+        data = json.load(json_file)
+
+    correct = 0
+    incorrect = 0
+    incorrect_video_names = []
+
+    # 遍历所有的日志条目
+    for video_name, video_data in data.items():
+        category = video_data['result']['category']
+
+        # 在视频名字中找到对应的数字
+        category_num = int(video_name.split('_')[3])
+
+        # 检查category的值是否与视频名字中的数字对应
+        if (category_num == 31 and category != 0) or (category_num == 30 and category != 3):
+            incorrect += 1
+            incorrect_video_names.append(video_name)
+        else:
+            correct += 1
+
+    print(f"正确的个数: {correct}")
+    print(f"错误的个数: {incorrect}")
+    if incorrect > 0:
+        print("错误的视频名称:")
+        for name in incorrect_video_names:
+            print(name)
+
+# 运行函数
+check_video_logs(path)
+
+# call the function with your json file
