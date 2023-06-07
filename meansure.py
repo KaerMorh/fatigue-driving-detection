c6904f87f8f10b2d1fde595518c95682f7b5abef
KaerMorh <1830616305@qq.com>
2023-06-07 17:42:59 +0800

    Caculate F1-Score

--- meansure.py
+++ meansure.py
@@ -51,7 +51,73 @@ def check_video_logs(path,mode = 0):
         for name in incorrect_video_names:
             print(name)
 
+
+def true_cls(line):
+    category_num = int(line.split('_')[3])
+    if (category_num == 31 or category_num == 21 or category_num == 11 or category_num == 41):
+        return 0
+    if category_num == 30:
+        return 3
+    if category_num == 20:
+        return 2
+    if category_num == 10:
+        return 1
+    if category_num == 40:
+        return 4
+
+
+def calculate_score(filename):
+    # 类别的总数
+    total_counts = [9+14+8+9+36, 11, 8, 18, 6+4+1+4+5+7]
+
+    # 初始化计数器
+    error_counts = [0, 0, 0, 0, 0]
+    false_positive = [0, 0, 0, 0, 0]
+    false_negative = [0, 0, 0, 0, 0]
+
+    with open(filename, 'r') as file:
+        for line in file:
+            # 解析文件名
+            fields = line.strip().split('_')
+            true_class = true_cls(line)
+            predicted_class = int(fields[-1][-1])
+
+            if true_class != predicted_class:
+                false_positive[predicted_class] += 1
+                false_negative[true_class] += 1
+                error_counts[true_class] += 1
+
+    # 计算正确分类的视频数量
+    true_positive = [total - error for total, error in zip(total_counts, error_counts)]
+
+    # 计算每个类别的 F1-Score
+    f1_scores = []
+    for i in range(5):
+        if true_positive[i] + false_positive[i] != 0:
+            precision = true_positive[i] / (true_positive[i] + false_positive[i])
+        else:
+            precision = 0
+
+        if true_positive[i] + false_negative[i] != 0:
+            recall = true_positive[i] / (true_positive[i] + false_negative[i])
+        else:
+            recall = 0
+
+        if precision + recall != 0:
+            f1 = 2 * precision * recall / (precision + recall)
+        else:
+            f1 = 0
+
+        f1_scores.append(f1)
+
+    # 计算最后的得分
+    total_videos = sum(total_counts)
+    final_score = sum(f1 * count / total_videos for f1, count in zip(f1_scores, total_counts))
+
+    return final_score
+
+
 # 运行函数
 check_video_logs(path,1)
-
+print(calculate_score('file_list.txt'))
 # call the function with your json file
