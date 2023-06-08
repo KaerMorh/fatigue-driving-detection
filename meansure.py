d40141fb770a517d0a60694cfaf82a3f4b835a81
KaerMorh <1830616305@qq.com>
2023-06-08 11:44:03 +0800

    更新了meansure，新增了一个comparison函数，可以与file_list.txt中的错误项进行比较并给出两者的差异。

--- meansure.py
+++ meansure.py
@@ -5,9 +5,73 @@ path = r'D:\0---Program\Projects\aimbot\yolov5-master\yolov5-master\output'
 import json
 import glob
 import os
-def check_video_logs(path,mode = 0):
-    # 读取log文件
 
+
+def comparision(incorrect_videos, compare_path):
+    '''
+    输入一个列表incorrect_videos，和一个txt文件compare_path，先对incorrect_video的数据处理，将每一项的末尾数字存入categories中，
+    再生成一个新的列表videos_names，将incorrect_videos中的每一项的末尾数字去掉，存入videos_names中
+    从compare_path(一个txt文件，包含如night_man_002_10_1.mp40的列表)读取数据，将每一项的末尾数字存入compare_categories中，
+    再生成一个新的列表compare_videos，将compare_path中的每一项的末尾数字去掉，存入compare_videos中
+    将两个名称列表中的名称相同项对应的标签进行比较，如果不同则打印出来
+    如果有compare_videos中的名称不在videos_names中，则将其打印出来（少错误项）
+    如果有videos_names中的名称不在compare_videos中，则将其打印出来（新增错误项）
+    '''
+    with open(compare_path  , 'r') as file:
+        compare_videos = file.read().splitlines()
+    compare_categories = []
+    new_wrong = []
+    new_wrong_categories = []
+
+    for i in range(len(compare_videos)):
+        compare_categories.append(compare_videos[i][-1])
+        compare_videos[i] = compare_videos[i][:-1]
+    videos_names = []
+    categories = []
+    for i in range(len(incorrect_videos)):
+        categories.append(incorrect_videos[i][-1])
+        videos_names.append(incorrect_videos[i][:-1])
+    for i in range(len(videos_names)):
+        name = videos_names[i]
+        if videos_names[i] in compare_videos:
+            #将videos_names[i]在compare_videos中的位置存入com_cls
+            com_cls = compare_videos.index(videos_names[i])
+            a1 = compare_videos[com_cls]
+            a2 = compare_categories[com_cls]
+            if categories[i] != compare_categories[com_cls]:
+                # 打印出来名称，原标签，新标签，以一段字符形式
+                print('变动错误项',videos_names[i], '新标签：', categories[i], '原标签：',compare_categories[compare_videos.index(videos_names[i])])
+
+
+            else:
+                #打印出相同的错误，给出名称与标签
+                print('相同错误项：',videos_names[i],'标签：',categories[i])
+
+            #删除compare_videos中的相同项
+            compare_videos.pop(com_cls)
+            compare_categories.pop(com_cls)
+
+        else:
+            #加入到新增错误项中
+            new_wrong.append(videos_names[i])
+            new_wrong_categories.append(categories[i])
+    #打印出新增错误项,每行一个
+    for i in range(len(new_wrong)):
+        print('新增错误项：',new_wrong[i],'标签：',new_wrong_categories[i])
+
+
+    #打印出少错误项
+    for i in range(len(compare_videos)):
+        print('少错误项：',compare_videos[i],'标签：',compare_categories[i])
+
+
+
+
+
+
+
+
+def check_video_logs(path,mode = 0, compare_path = None ):
     if mode == 1:
         all_files = [file for file in glob.glob(os.path.join(path, '*.json'))]
         all_files.sort(key=os.path.getmtime)
@@ -25,9 +89,10 @@ def check_video_logs(path,mode = 0):
     for video_name, video_data in data.items():
         category = video_data['result']['category']
 
+
         # 在视频名字中找到对应的数字
         category_num = int(video_name.split('_')[3])
-
+        #将错误的视频加进to_compare_videos中
         # 检查category的值是否与视频名字中的数字对应
         if (category_num == 31 and category != 0) or (category_num == 30 and category != 3) or(category_num == 20 and category != 2) or (category_num == 31 and category != 0):
             incorrect += 1
@@ -43,6 +108,13 @@ def check_video_logs(path,mode = 0):
 
         else:
             correct += 1
+
+    if compare_path is not None:
+        try:
+            comparision(incorrect_video_names, compare_path)
+        except Exception as e:
+            print(e)
+
     print(f"file:{path}")
     print(f"正确的个数: {correct}")
     print(f"错误的个数: {incorrect}")
@@ -116,8 +188,35 @@ def calculate_score(filename):
 
     return final_score
 
+def calculate_average_duration(path, mode=0):
+    # 读取json文件
+    if mode == 1:
+        all_files = [file for file in glob.glob(os.path.join(path, '*.json'))]
+        all_files.sort(key=os.path.getmtime)
+        path = all_files[-1]  # use the latest file
+
+    with open(path) as json_file:
+        data = json.load(json_file)
+
+    total_duration = 0
+    video_count = 0
+
+    # 遍历所有的日志条目
+    for video_name, video_data in data.items():
+        duration = video_data['result']['duration']
+        total_duration += duration
+        video_count += 1
+
+    # 计算并返回平均用时
+    average_duration = total_duration / video_count if video_count > 0 else 0
+
+    return average_duration
+
+
 
 # 运行函数
-check_video_logs(path,1)
+# check_video_logs(path,1)
+check_video_logs(path,1,'file_list.txt')
+print(calculate_average_duration(path,1))
 print(calculate_score('file_list.txt'))
 # call the function with your json file
